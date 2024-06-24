b7c785461cd836dd873471134bd800f931a788cf
SilverCanvas72 <idewij27@igs.vic.edu.au>
2024-06-24 11:14:15 +1000

    Better movement.
    Player now continues to move side t side when the key is held down. Before you hade to press the key repeatedly.

--- main.py
+++ main.py
@@ -4,40 +4,57 @@ import sys
 # pygame setup
 pygame.init()
 
-screen_width = 400
-screen_height = 300
+screen_width = 800
+screen_height = 400
 x = 200
 y = 150
+velocity = 5
+x_change = 0
 
 screen = pygame.display.set_mode((screen_width, screen_height))
+pygame.display.set_caption('My first game')
 clock = pygame.time.Clock()
 baby_blue = (178, 185, 235)
 dino = pygame.image.load('assets/dino.png')
 direction = [0, 0]
+
+def player(x, y):
+
+    screen.blit(dino,(x, y))
 
 while True:
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
+
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_LEFT:
-                direction = [-10, 0]
-            elif event.key == pygame.K_RIGHT:
-                direction = [10, 0]
-            elif event.key == pygame.K_UP:
-                direction = [0, -10]        
-            elif event.key == pygame.K_DOWN:
-                direction = [0, 10]       
+                x_change = -5
+            if event.key == pygame.K_RIGHT:
+                x_change = 5
 
-        x += direction[0]
-        y += direction[1]
+    if event.type == pygame.KEYUP:
+        if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+            x_change = 0
 
-    
-    
+        #keys = pygame.key.get_pressed()
+
+        #if keys[pygame.K_LEFT]:
+            #x -= velocity
+
+        #if keys[pygame.K_RIGHT]:
+            #x += velocity
+
+        #elif keys[pygame.K_UP]:
+            #y -= velocity
+
+        #elif keys[pygame.K_DOWN]:
+            #y += velocity  
+    x += x_change
     screen.fill(baby_blue)
+    player(x, y)
 
-    screen.blit(dino,(x, y))
 
 
 
