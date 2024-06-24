4845065983842822bb3845fe698596f4be4d961c
SilverCanvas72 <idewij27@igs.vic.edu.au>
2024-06-24 12:10:52 +1000

    Collisions and Movement
    Movement system is now good and you can hold down all of the keys. Also got a collision box working so that the dino cannot move into it.

--- main.py
+++ main.py
@@ -6,16 +6,21 @@ pygame.init()
 
 screen_width = 800
 screen_height = 400
-x = 200
-y = 150
-velocity = 5
-x_change = 0
 
 screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption('My first game')
 clock = pygame.time.Clock()
 baby_blue = (178, 185, 235)
+
+x_change = 0
+y_change = 0
+obstacle = pygame.Rect(400,200, 80, 80)
+velocity = 5
 dino = pygame.image.load('assets/dino.png')
+rect = dino.get_rect()
+rect.x = 200
+rect.y = 150
+
 direction = [0, 0]
 
 def player(x, y):
@@ -30,33 +35,35 @@ while True:
 
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_LEFT:
-                x_change = -5
+                x_change = -velocity
             if event.key == pygame.K_RIGHT:
-                x_change = 5
+                x_change = velocity
+            if event.key == pygame.K_UP:
+                y_change = -velocity
+            if event.key == pygame.K_DOWN:
+                y_change = velocity
 
     if event.type == pygame.KEYUP:
         if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
             x_change = 0
+        if event.key == pygame.K_UP or event.key == pygame.K_DOWN:
+            y_change = 0        
 
-        #keys = pygame.key.get_pressed()
+    new_rect = rect.copy()
+    new_rect.x += x_change
+    new_rect.y += y_change
 
-        #if keys[pygame.K_LEFT]:
-            #x -= velocity
+    if not new_rect.colliderect(obstacle):
+        rect.x = new_rect.x
+        rect.y = new_rect.y
 
-        #if keys[pygame.K_RIGHT]:
-            #x += velocity
-
-        #elif keys[pygame.K_UP]:
-            #y -= velocity
-
-        #elif keys[pygame.K_DOWN]:
-            #y += velocity  
-    x += x_change
     screen.fill(baby_blue)
-    player(x, y)
-
-
-
+    pygame.draw.rect(screen, (58, 73, 92), obstacle)
+    screen.blit(dino, rect)
+    #pygame.draw.rect(screen, (0, 0, 0), pygame.Rect(x, y, 32, 32))
+    #player(x, y)
+        
+    screen.blit(dino, rect)
 
     pygame.display.flip()
 
